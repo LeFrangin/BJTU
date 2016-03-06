833ba98440bfb43713bd72f4caf7462bf576c407
Florian Guerin <florian.guerin@epitech.eu>
2016-03-06 21:36:14 +0800

    Switch to python 3.4

--- game-tools/week-1/client.py
+++ game-tools/week-1/client.py
@@ -9,7 +9,7 @@ def prompt() :
 if __name__ == "__main__":
      
     if(len(sys.argv) < 3) :
-        print 'Usage : python telnet.py hostname port'
+        print('Usage : python telnet.py hostname port')
         sys.exit()
      
     host = sys.argv[1]
@@ -22,10 +22,10 @@ if __name__ == "__main__":
     try :
         s.connect((host, port))
     except :
-        print 'Unable to connect'
+        print('Unable to connect')
         sys.exit()
      
-    print 'Connected to remote host. Start sending messages'
+    print('Connected to remote host. Start sending messages')
     prompt()
      
     while 1:
@@ -39,7 +39,7 @@ if __name__ == "__main__":
             if sock == s:
                 data = sock.recv(4096)
                 if not data :
-                    print '\nDisconnected from chat server'
+                    print('\nDisconnected from chat server')
                     sys.exit()
                 else :
                     #print data
